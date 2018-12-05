67a7d80003f35627922f48787959285d8b57aaf0
makardjian <makardjian@gmail.com>
2018-12-04 16:07:51 -0800

    finished basic requirements for promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,14 +10,49 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
+Promise.promisifyAll(fs);
+var promisification = require('./promisification');
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
-};
+  //use fs.readfile read readfilepath and extract first line to get username
+  // then do getGIthubProfileAync
+  // then use fs.writefile write to file 
+  return fs.readFileAsync(readFilePath, 'utf8')
+    .then(function(data) {
+      if (!data) {
+        throw new Error('read file path does not exist')
+      } else {
+        var username = data.split('\n')[0];
+        return username //Where does this username get returned to?
+      }
+    })
+    .then(function(username)  {
+      return promisification.getGitHubProfileAsync(username) 
+    }) 
+    .then(function(body) {
+      return fs.writeFileAsync(writeFilePath, JSON.stringify(body));  
+  });
+}  
 
 // Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
 };
+
+
+//Some Diferences b/w standard node callback signatures and promises:
+  //promises involve a lot of returning up a promise chain, where node uses 
+    //lots of callbacks to deliver information from one level to the next. 
+
+
+
+
+
+
+
+
+
+
+
+
